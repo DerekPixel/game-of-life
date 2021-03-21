92f1f8f312ae22869f343236c87f37605ad578c3
Derek Price <derekp098@gmail.com>
2021-03-21 14:10:41 -0700

    organized the code a bit and added a background grid

--- app.js
+++ app.js
@@ -1,194 +1,240 @@
-var rows = 50;
-var cols = 50;
 
-var grid = make2DArray(rows, cols);
-var nextGrid = make2DArray(rows, cols);
-
-var timer;
-var isPlaying = false;
-
-
-// console.table(grid);
-
-var mainDiv = document.getElementById('main');
-
-var canvas = document.createElement('canvas');
-canvas.id = 'mycanvas';
-canvas.width = 500;
-canvas.height = 500;
-canvas.style.backgroundColor = 'black';
-var ctx = canvas.getContext('2d');
-
-var playButton = document.createElement('button');
-playButton.textContent = 'play/pause';
-
-var stepButton = document.createElement('button');
-stepButton.textContent = 'step';
-
-var clearButton = document.createElement('button');
-clearButton.textContent = 'clear';
-
-
-mainDiv.append(canvas);
-mainDiv.append(playButton);
-mainDiv.append(stepButton);
-mainDiv.append(clearButton);
-
-playButton.addEventListener('click', () => {
-  if(isPlaying) {
-    clearInterval(timer);
-    isPlaying = false;
-  } else {
-    timer = setInterval(() => {
-      updateGridValues();
-    }, 100);
-    isPlaying = true;
-  }
-})
-
-stepButton.addEventListener('click', () => {
-  if(isPlaying) {
-    clearInterval(timer);
-    isPlaying = false;
-  } else {
-    updateGridValues();
-  }
-})
-
-clearButton.addEventListener('click', () => {
-  isPlaying = false;
-  clearInterval(timer);
-  grid = make2DArray(rows, cols);
-})
-
-var xOffset = canvas.width / cols;
-var yOffset = canvas.height / rows;
-
-
-loop();
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  for(var i = 0; i < grid.length; i++) {
-    for(var j = 0; j < grid[i].length; j++) {
-      if(grid[i][j] === 1) {
-        ctx.save();
-        // ctx.scale(5, 5);
-        ctx.translate(j * xOffset, i * yOffset);
-        ctx.beginPath();
-        ctx.rect(0, 0, xOffset, yOffset)
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.restore();
-      }
-    }
-  }
-
-}
-
-function loop() {
-  window.requestAnimationFrame(loop);
-  draw();
-}
-
-var rowState, colState;
-
-canvas.addEventListener('mousedown', (e) => {
-  handlePlaceCell(e);
-  canvas.addEventListener('mousemove', handlePlaceCell);
-});
-
-canvas.addEventListener('mouseup', () => {
-  canvas.removeEventListener('mousemove', handlePlaceCell);
-  rowState = undefined;
-  colState = undefined;
-});
-
-function handlePlaceCell(e) {
-  var rect = canvas.getBoundingClientRect();
-  var mouseX = e.clientX - rect.left;
-  var mouseY = e.clientY - rect.top;
-
-  var col = Math.floor(mouseX / xOffset);
-  var row = Math.floor(mouseY / yOffset);
-
-  if(rowState !== row || colState !== col) {
-    rowState = row;
-    colState = col;
-    placeCell(row, col);
-  }
-
-}
-
-function placeCell(row, col) {
+//CONTENTS
+/*
+  a1 - INITIALIZE HTML ELEMENTS AND GLOBAL VARIABLES
+    a2.1 - HTML ELEMENTS
+    a2.2 - GLOBAL VARIABLES
   
-  if(grid[row][col] === 1) {
-    grid[row][col] = 0;
-  } else if(grid[row][col] === 0) {
-    grid[row][col] = 1;
-  }
-}
+  b1 - APPEND HTML ELEMENTS
+
+  c1 - INITIALIZE EVENT LISTENERS
+    c2.1 - BUTTON LISTENERS
+    c2.2 - MOUSE LISTENERS
+
+  d1 - DRAW TO THE CANVAS
+    d2.1 - DRAW CELLS
+    d2.2 - DRAW THE BACKGROUND GRID
+
+  e1 - FUNCTIONS
+    e2.1 - make2DArray
+    e2.2 - countNeighbors
+    e2.3 - updateGridValues
+    e2.4 - placeCell
+*/
 
 
-function make2DArray(rows, cols) {
 
-  var arr = new Array(rows);
-  for(var i = 0; i < arr.length; i++) {
-    arr[i] = new Array(cols);
-  }
+//INITIALIZE HTML ELEMENTS AND GLOBAL VARIABLES - a1
 
-  for(var i = 0; i < arr.length; i++) {
-    for(var j = 0; j < arr[i].length; j++) {
-      // var oneOrZero = Math.random() > 0.8 ? 1 : 0;
-      arr[i][j] = 0;
+  //HTML ELEMENTS - a2.1
+  var mainDiv = document.getElementById('main');
+
+  var canvas = document.createElement('canvas');
+  canvas.id = 'mycanvas';
+  canvas.width = 500;
+  canvas.height = 500;
+  canvas.style.backgroundColor = 'black';
+  var ctx = canvas.getContext('2d');
+
+  var playButton = document.createElement('button');
+  playButton.textContent = 'play/pause';
+
+  var stepButton = document.createElement('button');
+  stepButton.textContent = 'step';
+
+  var clearButton = document.createElement('button');
+  clearButton.textContent = 'clear';
+
+  //GLOBAL VARIABLES - a2.2
+  var rows = 50;
+  var cols = 50;
+
+  var rowState, colState;
+
+  var xOffset = canvas.width / cols;
+  var yOffset = canvas.height / rows;
+
+  var grid = make2DArray(rows, cols);
+  var nextGrid = make2DArray(rows, cols);
+
+  var timer;
+  var isPlaying = false;
+
+//APPEND HTML ELEMENTS - b1
+  mainDiv.append(canvas);
+  mainDiv.append(playButton);
+  mainDiv.append(stepButton);
+  mainDiv.append(clearButton);
+
+//INITIALIZE EVENT LISTENERS - c1
+
+  //BUTTON LISTENERS - c2.1
+  playButton.addEventListener('click', () => {
+    if(isPlaying) {
+      clearInterval(timer);
+      isPlaying = false;
+    } else {
+      timer = setInterval(() => {
+        updateGridValues();
+      }, 100);
+      isPlaying = true;
     }
-  }
+  })
 
-  return arr;
-
-}
-
-function countNeighbors(grid, x, y) {
-  var total = 0;
-  for(var i = -1; i < 2; i++) {
-    var r = i + x;
-    if(r === -1) {
-      r = rows - 1;
-    } else if(r === rows) {
-      r = 0;
+  stepButton.addEventListener('click', () => {
+    if(isPlaying) {
+      clearInterval(timer);
+      isPlaying = false;
+    } else {
+      updateGridValues();
     }
-    for(var j = -1; j < 2; j++) {
-      var c = j + y;
-      if(c === -1) {
-        c = cols - 1;
-      } else if(c === cols) {
-        c = 0;
+  })
+
+  clearButton.addEventListener('click', () => {
+    isPlaying = false;
+    clearInterval(timer);
+    grid = make2DArray(rows, cols);
+  })
+
+  //MOUSE LISTENERS - c2.2
+  canvas.addEventListener('mousedown', (e) => {
+    placeCell(e);
+    canvas.addEventListener('mousemove', placeCell);
+  });
+
+  canvas.addEventListener('mouseup', () => {
+    canvas.removeEventListener('mousemove', placeCell);
+    rowState = undefined;
+    colState = undefined;
+  });
+
+//DRAW TO THE CANVAS - d1
+  loop();
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //DRAW CELLS - d2.1
+    for(var i = 0; i < grid.length; i++) {
+      for(var j = 0; j < grid[i].length; j++) {
+        if(grid[i][j] === 1) {
+          ctx.save();
+          ctx.translate(j * xOffset, i * yOffset);
+          ctx.beginPath();
+          ctx.rect(0, 0, xOffset, yOffset)
+          ctx.fillStyle = 'white';
+          ctx.fill();
+          ctx.restore();
+        }
       }
-      total += grid[r][c];
     }
+
+    //DRAW THE BACKGROUND GRID - d2.2
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    for(var i = 0; i < grid.length; i++) {
+      ctx.moveTo(0, i * xOffset);
+      ctx.lineTo(canvas.width, i * xOffset);
+      for(var j = 0; j < grid[i].length; j++) {
+        ctx.moveTo(j * yOffset, 0);
+        ctx.lineTo(j * yOffset, canvas.height);
+      }
+    }
+    ctx.strokeStyle = 'rgb(25, 25, 25)';
+    ctx.stroke();
   }
-  total -= grid[x][y];
-  return total;
-}
 
-function updateGridValues(){
+  function loop() {
+    window.requestAnimationFrame(loop);
+    draw();
+  }
 
-  for(var i = 0; i < nextGrid.length; i++) {
-    for(var j = 0; j < nextGrid[i].length; j++) {
-      var state = grid[i][j];
+//FUNCTIONS - e1
 
-      var neighbors = countNeighbors(grid, i, j);
+  //e2.1
+  function make2DArray(rows, cols) {
+    var arr = new Array(rows);
+    for(var i = 0; i < arr.length; i++) {
+      arr[i] = new Array(cols);
+    }
 
-      if(state === 0 && neighbors === 3) {
-        nextGrid[i][j] = 1;
-      } else if(state === 1 && (neighbors < 2 || neighbors > 3)) {
-        nextGrid[i][j] = 0;
-      } else {
-        nextGrid[i][j] = state;
+    for(var i = 0; i < arr.length; i++) {
+      for(var j = 0; j < arr[i].length; j++) {
+        // var oneOrZero = Math.random() > 0.8 ? 1 : 0;
+        arr[i][j] = 0;
+      }
+    }
+    return arr;
+  }
+
+  //e2.2
+  function countNeighbors(grid, x, y) {
+    var total = 0;
+
+    for(var i = -1; i < 2; i++) {
+      var r = i + x;
+
+      if(r === -1) {
+        r = rows - 1;
+      } else if(r === rows) {
+        r = 0;
+      }
+
+      for(var j = -1; j < 2; j++) {
+        var c = j + y;
+
+        if(c === -1) {
+          c = cols - 1;
+        } else if(c === cols) {
+          c = 0;
+        }
+
+        total += grid[r][c];
+      }
+    }
+
+    total -= grid[x][y];
+    return total;
+  }
+
+  //e2.3
+  function updateGridValues(){
+
+    for(var i = 0; i < nextGrid.length; i++) {
+      for(var j = 0; j < nextGrid[i].length; j++) {
+        var state = grid[i][j];
+
+        var neighbors = countNeighbors(grid, i, j);
+
+        if(state === 0 && neighbors === 3) {
+          nextGrid[i][j] = 1;
+        } else if(state === 1 && (neighbors < 2 || neighbors > 3)) {
+          nextGrid[i][j] = 0;
+        } else {
+          nextGrid[i][j] = state;
+        }
+      }
+    }
+
+    [grid, nextGrid] = [nextGrid, grid];
+  }
+
+  //e2.4
+  function placeCell(e) {
+    var rect = canvas.getBoundingClientRect();
+    var mouseX = e.clientX - rect.left;
+    var mouseY = e.clientY - rect.top;
+
+    var col = Math.floor(mouseX / xOffset);
+    var row = Math.floor(mouseY / yOffset);
+
+    if(rowState !== row || colState !== col) {
+      rowState = row;
+      colState = col;
+
+      if(grid[row][col] === 1) {
+        grid[row][col] = 0;
+      } else if(grid[row][col] === 0) {
+        grid[row][col] = 1;
       }
     }
   }
-
-  [grid, nextGrid] = [nextGrid, grid];
-
-}
